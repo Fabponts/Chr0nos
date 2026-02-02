512ef4dbed4dd51ac2dfa2c81aa3e8d34d3dc3c8
Fabpont <fabriciopontes02.fa@gmail.com>
2026-02-02 17:42:04 -0300

    feat(RegistroPontoService): Create a service class to implement the rules and creation about clock in

---
+++ src/main/java/com/example/chr0nos/service/RegistroPontoService.java
@@ -0,0 +1,10 @@
+package com.example.chr0nos.service;
+
+import com.example.chr0nos.repository.FuncionarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistroPontoService {
+
+
+}
