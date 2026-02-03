5ad7ea1bb92e9fb6612fe5aa1190d39ab97d0aa3
Fabpont <fabriciopontes02.fa@gmail.com>
2026-02-03 15:37:33 -0300

    feat(Layout): Add the page structure and style using HTML and Css

--- src/main/java/com/example/chr0nos/DTO/RegistroPontoDTO.java
+++ src/main/java/com/example/chr0nos/DTO/RegistroPontoDTO.java
@@ -1,10 +1,16 @@
 package com.example.chr0nos.DTO;
 
+import com.example.chr0nos.model.Funcionario;
 import com.example.chr0nos.model.TipoRegistro;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class RegistroPontoDTO {
     //Dtos da classe
     private TipoRegistro tipoRegistro;
+    private Long funcionarioId;
+    private LocalDateTime horario;
 
     //Getters and Setters dos DTOS
     public TipoRegistro getTipoRegistro() {
@@ -13,5 +19,21 @@ public class RegistroPontoDTO {
 
     public void setTipoRegistro(TipoRegistro tipoRegistro) {
         this.tipoRegistro = tipoRegistro;
+    }
+
+    public Long getFuncionarioId() {
+        return funcionarioId;
+    }
+
+    public void setFuncionarioId(Long funcionarioId) {
+        this.funcionarioId = funcionarioId;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
     }
 }
--- src/main/java/com/example/chr0nos/service/RegistroPontoService.java
+++ src/main/java/com/example/chr0nos/service/RegistroPontoService.java
@@ -1,10 +1,20 @@
 package com.example.chr0nos.service;
 
+
 import com.example.chr0nos.repository.FuncionarioRepository;
+import com.example.chr0nos.repository.RegistroPontoRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class RegistroPontoService {
+    private final RegistroPontoRepository registroPontoRepository;
+    private final FuncionarioRepository funcionarioRepository;
 
+    public RegistroPontoService(RegistroPontoRepository registroPontoRepository,  FuncionarioRepository funcionarioRepository) {
+        this.registroPontoRepository = registroPontoRepository;
+        this.funcionarioRepository = funcionarioRepository;
+    }
 
 }
